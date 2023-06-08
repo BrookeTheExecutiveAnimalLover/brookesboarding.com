8b70d29ea42233c895872ad8d700baf00950df89
Brooke Burton <brooke@brookesboarding.com>
2023-06-08 00:21:23 -0700

    fixed header spacing bug

--- brookes-boarding/src/index.js
+++ brookes-boarding/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./Components/Hero/hero.css";
-import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Components/Hero/hero.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
